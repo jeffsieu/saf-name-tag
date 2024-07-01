0b396e033e52a7237107d1e67b9cc61ce7c68ba2
Jeff Sieu <jeffsy00@gmail.com>
2024-07-01 17:07:47 +0800

    feat: add source code link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,6 +38,7 @@ import { rulesByNameType } from "@/utils/rules";
 import { exampleNames } from "@/utils/examples";
 import parseName from "@/utils/parseName";
 import { NameType } from "@/utils/types";
+import { cn } from "@/lib/utils";
 
 const formSchema = z.object({
   name: z.string(),
@@ -91,7 +92,7 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-4">
-      <div className="flex flex-col gap-8 max-w-3xl pt-8">
+      <div className="flex flex-col gap-16 max-w-3xl pt-16">
         <div className="space-y-4">
           <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
             SAF Name Tag Calculator
@@ -101,137 +102,111 @@ export default function Home() {
             the latest name tag conventions.
           </p>
         </div>
-        <Form {...form}>
-          <form className="grid space-y-8">
-            <FormField
-              control={form.control}
-              name="name"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Full name</FormLabel>
-                  <FormControl>
-                    <Input
-                      className="uppercase"
-                      autoComplete="off"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormDescription>
-                    This is your name according to your NRIC.
-                  </FormDescription>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="type"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Name Type</FormLabel>
-                  <FormControl>
-                    <Select
-                      {...field}
-                      value={field.value}
-                      onValueChange={(value) => {
-                        field.onChange(value as NameType);
-                      }}
-                    >
-                      <SelectTrigger className="w-[180px]">
-                        <SelectValue placeholder="Select name type" />
-                      </SelectTrigger>
-                      <SelectContent>
-                        <SelectItem value="chineseEnglish">
-                          Chinese/English
-                        </SelectItem>
-                        <SelectItem value="malay">Malay</SelectItem>
-                        <SelectItem value="indian">Indian</SelectItem>
-                      </SelectContent>
-                    </Select>
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="surnameIndices"
-              render={() => (
-                <FormItem>
-                  <div className="grid space-y-1.5 mb-4">
-                    <FormLabel>Surname/s</FormLabel>
-                    <FormDescription>
-                      Select the surname/s in your name. Include Bin, Bte, D/O,
-                      S/O if applicable.
-                    </FormDescription>
-                  </div>
-                  {parsedName.names.map((name, index) => (
-                    <FormField
-                      key={index.toString()}
-                      control={form.control}
-                      name="surnameIndices"
-                      render={({ field }) => (
-                        <FormItem className="flex flex-row items-start space-y-0 space-x-2">
-                          <FormControl>
-                            <Checkbox
-                              checked={field.value.includes(index)}
-                              onCheckedChange={(checked) => {
-                                if (checked) {
-                                  field.onChange([...field.value, index]);
-                                } else {
-                                  field.onChange(
-                                    field.value?.filter(
-                                      (value) => value !== index
-                                    )
-                                  );
-                                }
-                              }}
-                            />
-                          </FormControl>
-                          <div className="grid gap-1.5 leading-none">
-                            <FormLabel>{name.toUpperCase()}</FormLabel>
-                            <FormMessage />
-                          </div>
-                        </FormItem>
-                      )}
-                    />
-                  ))}
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <Separator />
-            <FormField
-              control={form.control}
-              name="isDoctor"
-              render={({ field }) => (
-                <FormItem className="flex flex-row items-start space-y-0 space-x-2">
-                  <FormControl>
-                    <Checkbox
-                      checked={field.value}
-                      onCheckedChange={(checked) => {
-                        const isChecked = checked === true;
-                        if (isChecked) {
-                          form.setValue("isMdes", false);
-                        }
-                        field.onChange(isChecked);
-                      }}
-                    />
-                  </FormControl>
-                  <div className="grid gap-1.5 leading-none">
-                    <FormLabel>I am a doctor</FormLabel>
-                    <FormDescription>
-                      This will add a DR to your name.
-                    </FormDescription>
-                    <FormMessage />
-                  </div>
-                </FormItem>
-              )}
-            />
-            <div className="flex flex-col gap-4">
+        <section>
+          <Form {...form}>
+            <form className="grid space-y-8">
               <FormField
                 control={form.control}
-                name="isMdes"
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Full name</FormLabel>
+                    <FormControl>
+                      <Input
+                        className="uppercase"
+                        autoComplete="off"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormDescription>
+                      Your name according to your NRIC.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="type"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name type</FormLabel>
+                    <FormControl>
+                      <Select
+                        {...field}
+                        value={field.value}
+                        onValueChange={(value) => {
+                          field.onChange(value as NameType);
+                        }}
+                      >
+                        <SelectTrigger className="w-[180px]">
+                          <SelectValue placeholder="Select name type" />
+                        </SelectTrigger>
+                        <SelectContent>
+                          <SelectItem value="chineseEnglish">
+                            Chinese/English
+                          </SelectItem>
+                          <SelectItem value="malay">Malay</SelectItem>
+                          <SelectItem value="indian">Indian</SelectItem>
+                        </SelectContent>
+                      </Select>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="surnameIndices"
+                render={() => (
+                  <FormItem>
+                    <div className="grid space-y-1.5 mb-4">
+                      <FormLabel>Surname/s</FormLabel>
+                      <FormDescription>
+                        Your surname or family name. Include Bin, Bte, D/O, S/O
+                        if applicable.
+                      </FormDescription>
+                    </div>
+                    {parsedName.names.map((name, index) => (
+                      <FormField
+                        key={index.toString()}
+                        control={form.control}
+                        name="surnameIndices"
+                        render={({ field }) => (
+                          <FormItem className="flex flex-row items-start space-y-0 space-x-2">
+                            <FormControl>
+                              <Checkbox
+                                checked={field.value.includes(index)}
+                                onCheckedChange={(checked) => {
+                                  if (checked) {
+                                    field.onChange([...field.value, index]);
+                                  } else {
+                                    field.onChange(
+                                      field.value?.filter(
+                                        (value) => value !== index
+                                      )
+                                    );
+                                  }
+                                }}
+                              />
+                            </FormControl>
+                            <div className="grid gap-1.5 leading-none">
+                              <FormLabel>{name.toUpperCase()}</FormLabel>
+                              <FormMessage />
+                            </div>
+                          </FormItem>
+                        )}
+                      />
+                    ))}
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Separator />
+              <div className="text-lg font-semibold">Additional info</div>
+              <FormField
+                control={form.control}
+                name="isDoctor"
                 render={({ field }) => (
                   <FormItem className="flex flex-row items-start space-y-0 space-x-2">
                     <FormControl>
@@ -240,50 +215,79 @@ export default function Home() {
                         onCheckedChange={(checked) => {
                           const isChecked = checked === true;
                           if (isChecked) {
-                            form.setValue("isDoctor", false);
+                            form.setValue("isMdes", false);
                           }
                           field.onChange(isChecked);
                         }}
                       />
                     </FormControl>
                     <div className="grid gap-1.5 leading-none">
-                      <FormLabel>I am under the MDES scheme</FormLabel>
+                      <FormLabel>I am a doctor</FormLabel>
                       <FormDescription>
-                        This will add your MDES rank to your name.
+                        This will add a DR to your name.
                       </FormDescription>
                       <FormMessage />
                     </div>
                   </FormItem>
                 )}
               />
-              <FormField
-                control={form.control}
-                name="rank"
-                render={({ field }) => (
-                  <FormItem className="ms-6">
-                    <FormLabel>MDES rank</FormLabel>
-                    <FormControl>
-                      <Input
-                        placeholder="E.g. ME1T, ME4-1,
+              <div className="flex flex-col gap-4">
+                <FormField
+                  control={form.control}
+                  name="isMdes"
+                  render={({ field }) => (
+                    <FormItem className="flex flex-row items-start space-y-0 space-x-2">
+                      <FormControl>
+                        <Checkbox
+                          checked={field.value}
+                          onCheckedChange={(checked) => {
+                            const isChecked = checked === true;
+                            if (isChecked) {
+                              form.setValue("isDoctor", false);
+                            }
+                            field.onChange(isChecked);
+                          }}
+                        />
+                      </FormControl>
+                      <div className="grid gap-1.5 leading-none">
+                        <FormLabel>I am under the MDES scheme</FormLabel>
+                        <FormDescription>
+                          This will add your MDES rank to your name.
+                        </FormDescription>
+                        <FormMessage />
+                      </div>
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="rank"
+                  render={({ field }) => (
+                    <FormItem className="ms-6">
+                      <FormLabel>MDES rank</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="E.g. ME1T, ME4-1,
                       ME7"
-                        className="uppercase"
-                        autoComplete="off"
-                        disabled={!isMdes}
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormDescription>
-                      This is your full MDES rank. For example, ME1T, ME4-1,
-                      ME7.
-                    </FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </div>
-          </form>
-        </Form>
-        <section className="grid space-y-2">
+                          className="uppercase"
+                          autoComplete="off"
+                          disabled={!isMdes}
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormDescription>
+                        This is your full MDES rank. For example, ME1T, ME4-1,
+                        ME7.
+                      </FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+            </form>
+          </Form>
+        </section>
+        <section className="grid space-y-4">
           <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
             Results
           </h2>
@@ -323,7 +327,7 @@ export default function Home() {
             ))}
           </div>
         </section>
-        <section className="grid space-y-2">
+        <section className="grid space-y-4">
           <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
             Rules
           </h2>
@@ -333,7 +337,7 @@ export default function Home() {
             ))}
           </ul>
         </section>
-        <section className="grid space-y-2">
+        <section className="grid space-y-4">
           <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
             Examples
           </h2>
@@ -359,11 +363,20 @@ export default function Home() {
             </TableBody>
           </Table>
         </section>
-        <p className="text-muted-foreground text-sm">
-          Disclaimer: This application is for educational purposes only. It is a
-          personal project and is not an official tool associated with/developed
-          by the SAF.
-        </p>
+        <section className="grid space-y-4">
+          <p className="text-muted-foreground text-sm">
+            Disclaimer: This application is for educational purposes only. It is
+            a personal project and is not an official tool associated
+            with/developed by the SAF.{" "}
+          </p>
+          <a
+            className="text-muted-foreground text-sm underline"
+            href="https://github.com/jeffsieu/saf-name-tag"
+            target="_blank"
+          >
+            Source code
+          </a>
+        </section>
       </div>
     </main>
   );
